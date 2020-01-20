5cb964508820b3d336760cf4bf20b3b9f5cc7be3
Lukasz Macon <macon.lukasz@wp.pl>
2020-01-20 23:44:54 +0100

    Updates

---
+++ precache-manifest.345178202a3de2b4daefe91b52b1463d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "17693f6582a5747efe9d",
+    "url": "/react-app/static/css/main.9188e6e1.chunk.css"
+  },
+  {
+    "revision": "17693f6582a5747efe9d",
+    "url": "/react-app/static/js/main.1f260d6b.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "900236b148a2e688eac6",
+    "url": "/react-app/static/js/2.da199bdc.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "7f4157ea8d117e446280ee1aae13170d",
+    "url": "/react-app/index.html"
+  }
+];
